8681e87f73a3bbca27989a9017d95db87fa9ac6d
Ferry Hasan <ferryhasanbackup@gmail.com>
2026-01-02 10:11:51 +0700

    feat: Add face recognition and registration screens

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'HomeScreen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen()),
+  );
+}
